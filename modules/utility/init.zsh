0860af8c8d6ec75a1603ef1d6137e85c661ed8f3
Jack Lee <jack.y.l.dev@gmail.com>
2024-06-07 03:27:44 -0400

    updates
    - update yabai layout script
    - update zellij layout
    - update zsh aliases
    - update karabiner config
    - update languages.toml

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -119,7 +119,6 @@ alias lr='ll -R'         # Lists human readable sizes, recursively.
 alias la='ll -a'         # Lists human readable sizes, hidden files.
 alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
 alias lk='ll -Sr'        # Lists sorted by size, largest last.
-alias lt='ll -tr'        # Lists sorted by date, most recent last.
 alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
 alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
 
